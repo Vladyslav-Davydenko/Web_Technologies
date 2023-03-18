faa5c2f631eebd02c7ea3917cd8bea89aef723c8
Mariia Boiko <marboi@ttu.ee>
2023-03-18 13:23:21 +0200

    template in main_paige.php

--- main_paige.php
+++ main_paige.php
@@ -9,147 +9,16 @@
                     </div>
                     <div class="post-author-text">
                         <a href="profile.html">
-                            <h4>by Jone Doe</h4>
+                            <h4>{USERNAME}</h4>
                         </a>
                     </div>
                 </div>
-                <h3>Gawai</h3>
+                <h3>{PLACE}</h3>
                 <div class="detail-info-post">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
+                    <p>{TEXT}</p>
                 </div>
                 <div class="posted">
-                    <h4>posted 2 days ago</h4>
-                </div>
-            </div>
-        </div>
-
-        <div class="single-post">
-            <a href="single_post.html"><img src="img/posts/italy.jpeg"></a>
-            <div class="post-text">
-                <div class="post-author">
-                    <div class="post-author-img">
-                        <a href="profile.html"><img class="avatar-small" src="img/avatars/kiril.jpg"></a>
-                    </div>
-                    <div class="post-author-text">
-                        <a href="profile.html">
-                            <h4>by Jone Doe</h4>
-                        </a>
-                    </div>
-                </div>
-                <h3>Italy</h3>
-                <div class="detail-info-post">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                </div>
-                <div class="posted">
-                    <h4>posted 2 days ago</h4>
-                </div>
-            </div>
-        </div>
-        <div class="single-post">
-            <a href="single_post.html"><img src="img/posts/island.jpeg"></a>
-            <div class="post-text">
-                <div class="post-author">
-                    <div class="post-author-img">
-                        <a href="profile.html"><img class="avatar-small" src="img/avatars/kiril.jpg"></a>
-                    </div>
-                    <div class="post-author-text">
-                        <a href="profile.html">
-                            <h4>by Jone Doe</h4>
-                        </a>
-                    </div>
-                </div>
-                <h3>Ireland</h3>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                </p>
-                <div class="posted">
-                    <h4>posted 2 days ago</h4>
-                </div>
-            </div>
-        </div>
-        <div class="single-post">
-            <a href="single_post.html"><img src="img/posts/german.jpeg"></a>
-            <div class="post-text">
-                <div class="post-author">
-                    <div class="post-author-img">
-                        <a href="profile.html"><img class="avatar-small" src="img/avatars/masha.jpg"></a>
-                    </div>
-                    <div class="post-author-text">
-                        <a href="profile.html">
-                            <h4>by Jone Doe</h4>
-                        </a>
-                    </div>
-                </div>
-                <h3>German</h3>
-                <div class="detail-info-post">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                </div>
-                <div class="posted">
-                    <h4>posted 2 days ago</h4>
-                </div>
-            </div>
-        </div>
-        <div class="single-post">
-            <a href="single_post.html"><img src="img/posts/singapore.jpeg"></a>
-            <div class="post-text">
-                <div class="post-author">
-                    <div class="post-author-img">
-                        <a href="profile.html"><img class="avatar-small" src="img/avatars/Visl.jpg"></a>
-                    </div>
-                    <div class="post-author-text">
-                        <a href="profile.html">
-                            <h4>by Jone Doe</h4>
-                        </a>
-                    </div>
-                </div>
-                <h3>Singapore</h3>
-                <div class="detail-info-post">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                </div>
-                <div class="posted">
-                    <h4>posted 2 days ago</h4>
-                </div>
-            </div>
-        </div>
-        <div class="single-post">
-            <a href="single_post.html"><img src="img/posts/france.jpeg"></a>
-            <div class="post-text">
-                <div class="post-author">
-                    <div class="post-author-img">
-                        <a href="profile.html"><img class="avatar-small" src="img/avatars/masha.jpg"></a>
-                    </div>
-                    <div class="post-author-text">
-                        <a href="profile.html">
-                            <h4>by Jone Doe</h4>
-                        </a>
-                    </div>
-                </div>
-                <h3>France</h3>
-                <div class="detail-info-post">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                </div>
-                <div class="posted">
-                    <h4>posted 2 days ago</h4>
+                    <h4>{POSTED TIME}</h4>
                 </div>
             </div>
         </div>
