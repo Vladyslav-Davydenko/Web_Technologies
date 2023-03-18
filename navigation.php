3d095ec66694896c308931c399ca4d0381151527
Mariia Boiko <marboi@ttu.ee>
2023-03-18 12:34:58 +0200

    index.php easier

---
+++ navigation.php
@@ -0,0 +1,34 @@
+<div class="hamberger-menu">
+                <input id="menu-switch" type="checkbox" />
+                <label class="menu-btn" for="menu-switch">
+                    <span></span>
+                </label>
+
+                <ul class="menu-box">
+                    <li><a class="menu-item" href="index.html">Home</a></li>
+                    <li><a class="menu-item" href="profile.html">Profile</a></li>
+                    <li><a class="menu-item" href="about.html">About</a></li>
+                    <li><a class="menu-item" href="registration.html">Register</a></li>
+                    <li><a class="menu-item" href="login.html">Login</a></li>
+                </ul>
+            </div>
+            <div class="nav-link">
+                <a class="home-link" href="index.html">Home</a>
+                <a class="about-link" href="about.html">About</a>
+
+                <a class="login-link" href="login.html">Login</a>
+                <a class="profile-link" href="profile.html"><svg style="color: rgb(25, 46, 94);"
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" fill="#192e5e"></path>
+                        <path fill-rule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                            fill="#192e5e"></path>
+                    </svg></a>
+                <form class="form" action="#" method="get">
+                    <div class="form-field">
+                        <input class="input-text" type="text" name="text" placeholder="Search by Post Title" />
+                    </div>
+                    <input class="btn" type="submit" value="Search" />
+                </form>
+            </div>
