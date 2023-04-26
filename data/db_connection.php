97d0cd7e2b6fc2444797e688cfba28ea034e7aed
kiboik <boykokirill04@gmail.com>
2023-04-26 19:59:57 +0300

    Data for connection to DB

--- data/db_connection.php
+++ data/db_connection.php
@@ -1,6 +1,6 @@
 <?php 
-	$server = "localhost";    // $server = "anysql.itcollege.ee";
-	$user = "root";			  // $user = "ICS0008_WT_5";
-	$password = "";			  // $password = "30e65476bacf";
-	$database = "course_db"; // $database = "ICS0008_5";
+	$server = "anysql.itcollege.ee";    // $server = "anysql.itcollege.ee";
+	$user = "ICS0008_WT_5";			  // $user = "ICS0008_WT_5";
+	$password = "30e65476bacf";			  // $password = "30e65476bacf";
+	$database = "ICS0008_5"; // $database = "ICS0008_5";
 ?>
